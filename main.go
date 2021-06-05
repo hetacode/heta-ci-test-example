9d5c2743eb58db4c4f4117bb9ec5de240a07429a
Sebastian G <38227216+hetacode@users.noreply.github.com>
2021-06-05 11:47:29 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	hostanme, _ := os.Hostname()
-	fmt.Println("develop")
+	fmt.Println("develop release")
 	fmt.Println("hostname: " + hostanme)
 	fmt.Println("from develop branch")
 }
